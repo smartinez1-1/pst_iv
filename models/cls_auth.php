b6a6b5a41c3bd045a91c6b63606697965f184d58
19yisus <jesusitomorales70@gmail.com>
2023-04-19 17:56:26 -0430

    login(probado y funcionando), modulo carrera (trabajando, ya guarda)

--- models/cls_auth.php
+++ models/cls_auth.php
@@ -2,13 +2,13 @@
   if(!class_exists("cls_db")) require("cls_db.php");
 
   class cls_auth extends cls_db{
-    private $cedula_usuario, $cedula, $clase_usuario, $pregunta1, $pregunta2, $pregunta3, $respuesta1, $respuesta2, $respuesta3;
+    private $cedula_usuario, $cedula, $clave_usuario, $pregunta1, $pregunta2, $pregunta3, $respuesta1, $respuesta2, $respuesta3;
 
     public function __construct(){
       parent::__construct();
       $this->cedula_usuario = null;
       $this->cedula = null;
-      $this->clase_usuario = null;
+      $this->clave_usuario = null;
       $this->pregunta1 = null;
       $this->pregunta2 = null;
       $this->respuesta1 = null;
@@ -17,7 +17,7 @@
     
     public function SetDatos($d){
       $this->cedula_usuario = isset($d['cedula_usuario']) ? $d['cedula_usuario'] : null;
-      $this->clase_usuario = isset($d['clase_usuario']) ? $d['clase_usuario'] : null;
+      $this->clave_usuario = isset($d['clave_usuario']) ? $d['clave_usuario'] : null;
       $this->pregunta1 = isset($d['pregunta1']) ? $d['pregunta1'] : null;
       $this->pregunta2 = isset($d['pregunta2']) ? $d['pregunta2'] : null;
       $this->pregunta3 = isset($d['pregunta3']) ? $d['pregunta3'] : null;
@@ -27,13 +27,13 @@
     }
     
     public function Login(){
-      $res = $this->Query("SELECT * FROM usuarios WHERE cedula_usuario = '$this->cedula_usuario' ;");
+      $res = $this->Query("SELECT * FROM usuario WHERE cedula_usuario = '$this->cedula_usuario' ;");
       
       if($res->num_rows > 0){
         $datos = $this->Get_array($res);
         if($datos['estatus_usuario'] === "0") return [false,"err/06AUTH"];
-
-        if(password_verify($this->password, $datos['clave_usuario'])){
+        
+        if(password_verify($this->clave_usuario, $datos['clave_usuario'])){
           session_start();
           $_SESSION['cedula'] = $datos['cedula_usuario'];
           $_SESSION['username'] = $datos['nombre_usuario'];
