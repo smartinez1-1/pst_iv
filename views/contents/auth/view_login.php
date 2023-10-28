424f2ddf7a35f33422a997cf4045f88cf2027894
Gabriel Valera <gabrielj2814@hotmail.com>
2023-10-28 13:00:38 -0400

    change configurando conexion

--- views/contents/auth/view_login.php
+++ views/contents/auth/view_login.php
@@ -67,7 +67,7 @@
 										<div class="mb-6">
 											<label class="mb-2.5 block font-medium text-black dark:text-white">Contraseña</label>
 											<div class="relative">
-												<input type="password" required maxlength="12" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" minlength="8" placeholder="8+ Caracteres, 1 mayuscula, 1 numero, 1 caracter especial" name="clave_usuario"
+												<input type="password" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" minlength="8" placeholder="8+ Caracteres, 1 mayuscula, 1 numero, 1 caracter especial" name="clave_usuario"
 													class="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary" />
 
 												<span class="absolute right-4 top-4">
